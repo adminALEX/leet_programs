42baf04b8246df3438b533a35c46878f77a45bf6
ALEX S <alexflora6@gmail.com>
2023-02-26 21:08:33 +0530

    Time: 2 ms (57.28%), Space: 40.7 MB (23.01%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isValid(String st) {
+        Stack<Character> s = new Stack<>();
+        char[] ch = st.toCharArray();
+         if(st.length() == 0 || st.length() == 1) return false;
+        for(char c : ch){
+            if(c == '{' || c == '(' || c == '[')
+            {
+            s.push(c);
+            continue;
+            }
+            else if(c == '}'){
+                if(s.isEmpty() || s.pop() != '{')
+                return false;
+            }
+            else if(c == ')'){
+                if(s.isEmpty() || s.pop() != '(') return false;
+            }
+            else if(c == ']'){
+                if(s.isEmpty() || s.pop() != '[') return false;
+            }
+        }
+        return s.isEmpty();
+
+    }
+}
