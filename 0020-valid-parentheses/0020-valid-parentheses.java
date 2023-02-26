fac998bef5bfc53e634eb60123e0cee4b37152c1
ALEX S <alexflora6@gmail.com>
2023-02-26 21:09:18 +0530

    Time: 2 ms (57.28%), Space: 41 MB (22.19%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -4,14 +4,12 @@ class Solution {
         char[] ch = st.toCharArray();
          if(st.length() == 0 || st.length() == 1) return false;
         for(char c : ch){
-            if(c == '{' || c == '(' || c == '[')
-            {
-            s.push(c);
-            continue;
+            if(c == '{' || c == '(' || c == '['){
+                s.push(c);
+                continue;
             }
             else if(c == '}'){
-                if(s.isEmpty() || s.pop() != '{')
-                return false;
+                if(s.isEmpty() || s.pop() != '{') return false;
             }
             else if(c == ')'){
                 if(s.isEmpty() || s.pop() != '(') return false;
@@ -21,6 +19,5 @@ class Solution {
             }
         }
         return s.isEmpty();
-
     }
 }
