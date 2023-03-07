70231b6b9bac16b0eaddf558bd84bdb740e96f7d
ALEX S <alexflora6@gmail.com>
2023-03-07 23:09:16 +0530

    Time: 7 ms (62.90%), Space: 42.9 MB (26.04%) - LeetHub

--- 1529-minimum-suffix-flips/1529-minimum-suffix-flips.java
+++ 1529-minimum-suffix-flips/1529-minimum-suffix-flips.java
@@ -2,7 +2,8 @@ class Solution {
     public int minFlips(String target) {
         int count=0;
         char c = '0';
-        for(int i=0;i<target.length();i++){
+        int len = target.length();
+        for(int i=0;i<len;i++){
             if(target.charAt(i) == c)
                 continue;
             count++;
