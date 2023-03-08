6d72cb0c8b47da4bbf0500118751e060f22c75d0
ALEX S <alexflora6@gmail.com>
2023-03-08 10:09:42 +0530

    Time: 0 ms (100.00%), Space: 42.8 MB (24.05%) - LeetHub

--- 0832-flipping-an-image/0832-flipping-an-image.java
+++ 0832-flipping-an-image/0832-flipping-an-image.java
@@ -12,19 +12,5 @@ class Solution {
         }
         return res;
         
-        
-        
-        // for(int i=0;i<image.length;i++){
-        //     int first = 0, last=image[i].length-1;
-        //     for(int j = 0;j<=image[0].length/2;j++){
-        //         int t = image[i][first];
-        //         image[i][first] = image[i][last] == 0 ? 1:0;
-        //         image[i][last] = t == 0?1:0;
-        //         if(first>last) break;
-        //         first++;
-        //         last--;
-        //     }
-        // }
-        // return image;
     }
 }
